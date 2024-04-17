54e876266723924af7185b927d4fcd95c7b174f9
Dcode25 <deondre-mayers@hotmail.com>
2024-04-17 16:09:37 -0400

    fix properties

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PropertyController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
